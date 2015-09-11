dfa880981c84e12b07b9db374dfb88d152f545a8
dphaener <dphaener@gmail.com>
2015-09-11 14:45:44 -0700

    Add select to Array

---
+++ tests/array/select.js
@@ -0,0 +1,12 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+require('../../lib/array/select');
+
+describe('select', function() {
+  it('should return a new array for values that return true', function() {
+    var ary = [1, 2, 3, 4];
+
+    expect(ary.select(function(v) { return v === 3 })).to.eql([3]);
+  });
+});
