5d1feb36707f5e9c7d42e38cda6585a7ad833b2b
Zach <zackify@gmail.com>
2015-09-17 16:23:19 -0700

    cleaned up map

--- object/map.js
+++ object/map.js
@@ -1,9 +1,8 @@
 if (!Object.prototype.map) {
   Object.prototype.map = function(callback) {
-    var array = []
-    Object.keys(this).map((key, index) => {
-       array.push(callback(this[key], key, index))
+    var object = this
+    return Object.keys(this).map(function(key, index) {
+       return callback(object[key], key, index)
     });
-    return array
   }
 }
