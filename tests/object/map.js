84fa5d9f7705a9e9c28fcb8ce2776cab16d42604
Zach <zackify@gmail.com>
2015-09-18 13:30:53 -0700

    added test for .map

--- tests/object/map.js
+++ tests/object/map.js
@@ -12,4 +12,12 @@ describe('map', function() {
     expect(result[0]).to.eql('zach baller');
   })
 
+  it('should not modify the object', function() {
+    var object = {name: 'zach', full: 'zach silveira'}
+    var result = object.map(value => {
+      return value + ' baller'
+    })
+    expect(object).to.eql({name: 'zach', full: 'zach silveira'});
+  })
+
 });
